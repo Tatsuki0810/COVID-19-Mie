6bc5983a1b9171f1e4081ddfb39259b5f72fad2d
Tatsuki0810 <sss.53.88840@gmail.com>
2020-07-19 21:08:42 +0900

    changed

--- main.js
+++ main.js
@@ -6,14 +6,9 @@ request.open('GET', 'https://www.stopcovid19.jp/data/covid19japan.json', true);
 request.responseType = 'json';
 request.onload = function (){
     var data = this.response;
-    var npatients = data[23].npatients;
-    var ncurrentpatients = data[23].ncurrentpatients;
+    var npatients = getElementById("npatients");
+    npatients.innerHTML = data[23].npatients;
+    var ncurrentpatients = getElementById("ncurrentpatients");
+    ncurrentpatients.innerHTML = data[23].ncurrentpatients;
 };
- 
 request.send();
-
-$(function(){
-    $("#npatients").text(npatients);
-    $("#ncurrentpatients").text(ncurrentpatients);
-});
-
