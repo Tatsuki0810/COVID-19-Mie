7338dab0f4c9eb9b8caf32415debadd69d52469a
Tatsuki0810 <sss.53.88840@gmail.com>
2020-07-19 21:15:46 +0900

    changed

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var request = new XMLHttpRequest();
 var npatients = 0;
 var ncurrentpatients = 0;
@@ -5,7 +6,7 @@ var ncurrentpatients = 0;
 request.open('GET', 'https://www.stopcovid19.jp/data/covid19japan.json', true);
 request.responseType = 'json';
 request.onload = function (){
-    var data = this.response;
+    var data = JSON.parse(request.responseText);
     var npatients = getElementById("npatients");
     npatients.innerHTML = data[23].npatients;
     var ncurrentpatients = getElementById("ncurrentpatients");
