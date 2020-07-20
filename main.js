6f599d39d75817af9e35e75439ad09482b07e42c
Tatsuki0810 <sss.53.88840@gmail.com>
2020-07-20 20:46:16 +0900

    commit

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var request = new XMLHttpRequest();
-var npatients = 0;
-var ncurrentpatients = 0;
-
-request.open('GET', 'https://www.stopcovid19.jp/data/covid19japan.json', true);
-request.responseType = 'json';
-request.onload = function (){
-    var data = JSON.parse(request.response);
-    var npatients = getElementById("npatients");
-    npatients.innerHTML = data[23].npatients;
-    var ncurrentpatients = getElementById("ncurrentpatients");
-    ncurrentpatients.innerHTML = data[23].ncurrentpatients;
-};
-request.send();
+$(function() {
+      $.getJSON('https://covid19-japan-web-api.now.sh/api/v1/prefectures',)
+        .done(function(data) {
+            console.log(data.cases[24]);
+         });
+});
