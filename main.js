af4d69630c74d74f4e494114532bbec8a15106f8
Tatsuki0810 <sss.53.88840@gmail.com>
2020-07-20 20:52:10 +0900

    aaa

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-$(function() {
-      $.getJSON('https://covid19-japan-web-api.now.sh/api/v1/prefectures',)
-        .done(function(data) {
-            console.log(data.cases[24]);
-         });
-});
+fetch('https://covid19-japan-web-api.now.sh/api/v1/prefectures', {
+    headers: {
+      'Content-Type': 'pplication/json; charset=utf-8'
+    },
+    body: JSON.parse
+  })
+   
+    // 通信が成功したとき
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json);
+    })
+   
+    // 通信が失敗したとき
+    .catch(function(error) {
+      console.error('Error:', error);
+    });
