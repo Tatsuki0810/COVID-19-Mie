f70affe5e40958532f88f1eee7774b02b4907017
Tatsuki0810 <sss.53.88840@gmail.com>
2020-07-20 22:38:26 +0900

    Fixed

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 var ajax = new XMLHttpRequest();
  
-ajax.open("get", "https://www.stopcovid19.jp/data/covid19japan.json");
-ajax.send(); // 通信させます。
-ajax.addEventListener("load", function(){ // loadイベントを登録します。
-	console.log(this.response.area[23].npatients); // 通信結果を出力します。
+ajax.open("get", "https://covid19-japan-web-api.now.sh/api/v1/prefectures");
+ajax.send();
+ajax.addEventListener("load", function(){ 
+    var r = JSON.parse(this.response);
+    var total = document.getElementById("total");
+    total.innerHTML = r[23].cases;
+    var pcr = document.getElementById("pcr");
+    pcr.innerHTML = r[23].pcr;
+    var date = document.getElementById("date");
+    date.innerHTML = r[23].last_updated.cases_date;
 }, false);
