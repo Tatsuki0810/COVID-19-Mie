01b8eee60da34872d50b71113ac7b3407ddbc57f
Tatsuki0810 <sss.53.88840@gmail.com>
2020-07-20 21:20:00 +0900

    aaa

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var ajax = new XMLHttpRequest();
  
-ajax.open("get", "https://covid19-japan-web-api.now.sh/api/v1/prefectures");
+ajax.open("get", "https://www.stopcovid19.jp/data/covid19japan.json");
 ajax.send(); // 通信させます。
 ajax.addEventListener("load", function(){ // loadイベントを登録します。
 	console.log(this.response.area[23].npatients); // 通信結果を出力します。
